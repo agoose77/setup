434cc0cb8e88d0eed067e91fca5c8f63497398ee
Angus Hollands <goosey15@gmail.com>
2020-04-20 16:48:49 +0100

    Update setup

--- setup.py
+++ setup.py
@@ -322,6 +322,17 @@ function tkdir() {
             "nocd",
         ),
     )
+    # Add instant prompt
+    prepend_to_zshrc("""
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+    """)
+    # Finalise p10k. TODO - maintain representation of prep/app order in source to support ordering
+    append_to_zshrc("(( ! ${+functions[p10k]} )) || p10k finalize")
 
 
 def install_fd():
